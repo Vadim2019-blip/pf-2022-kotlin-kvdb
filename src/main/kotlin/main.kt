47d30bb8251ddd6f40daf82dc61b921f429c5c6e
Вадим <oxa-182008@yandex.ru>
2023-10-08 19:00:07 +0300

    Написал документацию

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,19 @@
 import java.io.File
+/*УТилита kvbd*/
+/*
+1)Как пользоваться:
+Первым пишется имя базы данных с которой будет дальней шая работа.
+В базе данных хранятся строки в виде key value, разделенные пробелом. Исходно есть файлик basa
+Утилита поддерживает операции:
+* `dbname put key value` — добавить в БД **dbname** по ключу **key** значение **value**
+* `dbname get  key` — получить из БД **dbname** текущее значение по ключу **key**
+* `dbname remove  key` — удалить из БД **dbname** текущее значение по ключу **key**
+ПРИМЕРЫ ВХОДНЫХ ДАННЫХ:
+basa get 1
+basa put apple 100
+*/
 
+/*Проверка на наличие файла*/
 fun isFileExists(file: File): Boolean {
     return file.exists() && !file.isDirectory
 }
@@ -11,8 +25,10 @@ fun main(args: Array<String>) {
         println("$file такого файла нету")
     }
     else {
+        /*Парсинг содержимого файла преобразование в map*/
         file_open.useLines { lines -> lines.forEach {input =  input + Pair(it.split(" ")[0], it.split(" ")[1])}}
     }
+    /*ДАлее идет основная часть программы с обработкой Бд и парсингом запроса*/
     if(args[1] == "put"){
         input += Pair(args[2], args[3])
         val key = args[2]
